14c3332c17bb1a1e1e39319a80ba0f4ad671fd3b
Kanuri, Kalyan (CW) <kalyan.kanuri1+hpi@hp.com>
2026-03-26 17:37:52 +0530

    comment error causing chan receiver

--- advanced/concurrency/channels.go
+++ advanced/concurrency/channels.go
@@ -17,7 +17,7 @@ func ChannelsInGo() {
 	msg := <-ch // received msg from channel
 	fmt.Println(msg)
 
-	msg = <-ch
+	// msg = <-ch
 	// if we try to receive another msg from the channel it will not give the same msg
 	// the current goroutine will wait forever for the other goroutine to send the msg
 	// but the goroutine is already completed it's execution so it results in deadlock
