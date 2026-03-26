3608b9bd13b5e898048311ecf95090fe754a9e2b
Kalyan.Kanuri <kalyankanuri497@gmail.com>
2026-03-26 07:07:32 +0530

    learn goroutines and it's execution

--- advanced/concurrency/goroutines.go
+++ advanced/concurrency/goroutines.go
@@ -2,9 +2,20 @@ package concurrency
 
 import (
 	"fmt"
+	"time"
 )
+
+func executeTask(msg string, delay time.Duration) {
+	fmt.Printf("Task execution start time %s\n", time.Now())
+	fmt.Printf("sleeping for %s time\n", delay)
+	time.Sleep(delay)
+	fmt.Printf("Task message: %s\n", msg)
+	fmt.Printf("Task execution end time %s\n", time.Now())
+}
 
 func GoRoutinesInGo() {
 	fmt.Println("-- Go Routines in Go --")
-	
+	go executeTask("Executing demo go routine", time.Second)
+	// we have to give sufficient time for go routine to complete
+	time.Sleep(2 * time.Second) // wait for go routine to complete
 }
