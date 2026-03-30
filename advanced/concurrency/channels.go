bc138ae1a0e608bfc8947e1beec088055562e0a6
Kanuri, Kalyan (CW) <kalyan.kanuri1+hpi@hp.com>
2026-03-30 16:39:20 +0530

    learn buffered unbuffered channels in go

--- advanced/concurrency/channels.go
+++ advanced/concurrency/channels.go
@@ -11,24 +11,21 @@ func sendMsg(ch chan string, chanType string) {
 
 func ChannelsInGo() {
 	fmt.Println("-- Channels In Go --")
-	ch := make(chan string) // unbuffered channel
+	ch := make(chan string)     // unbuffered channel
+	ch2 := make(chan string, 3) // Buffered channel with 3 blocks of memory
 	go sendMsg(ch, "unbuffered")
+	go sendMsg(ch2, "buffered")
 
-	msg := <-ch // received msg from channel
-	fmt.Println(msg)
-
+	select {
+	case msg := <-ch: // received msg from channel
+		fmt.Println(msg)
+		close(ch) // closing channel after work done
+	case msg2 := <-ch2:
+		fmt.Println(msg2)
+		close(ch2)
+	}
 	// msg = <-ch
 	// if we try to receive another msg from the channel it will not give the same msg
 	// the current goroutine will wait forever for the other goroutine to send the msg
 	// but the goroutine is already completed it's execution so it results in deadlock
-
-	ch2 := make(chan string, 3) // Buffered channel with 3 blocks of memory
-	go sendMsg(ch2, "buffered")
-
-	msg = <-ch2
-	fmt.Println(msg)
-
-	// closing a channel
-	close(ch)
-	close(ch2)
 }
