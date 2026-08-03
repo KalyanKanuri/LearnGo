afc952e3e8c3c8ff9f80d82c77539a8453914647
Kanuri, Kalyan (CW) <kalyan.kanuri1+hpi@hp.com>
2026-08-03 18:18:27 +0530

    [2026-08-03] learn encoding/json pkg and build employee manager project

---
+++ go-core/projects/emp_json_manager/go.mod
@@ -0,0 +1,3 @@
+module empjsonmgr
+
+go 1.26.2
---
+++ go-core/projects/emp_json_manager/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Address struct {
+	City    string `json:"city"`
+	Country string `json:"country"`
+}
+
+type Employee struct {
+	Name    string   `json:"name"`
+	Age     int      `json:"age"`
+	Address Address  `json:"address"`
+	Skills  []string `json:"skills"`
+}
+
+func main() {
+	jsonData, err := os.ReadFile("employees.json")
+	if err != nil {
+		fmt.Println("Error reading file", err)
+		return
+	}
+
+	var emps []Employee
+	err = json.Unmarshal(jsonData, &emps)
+	if err != nil {
+		fmt.Println("Error unmarshaling json data", err)
+		return
+	}
+
+	fmt.Println("============ Employee Summary ============")
+	for i, emp := range emps {
+		fmt.Printf("%d. %s\nAge: %d\nCity: %s\nCountry: %s\nSkills: %s\n\n",
+			i+1, emp.Name, emp.Age, emp.Address.City, emp.Address.Country, emp.Skills[:],
+		)
+	}
+	fmt.Println("==========================================")
+}
