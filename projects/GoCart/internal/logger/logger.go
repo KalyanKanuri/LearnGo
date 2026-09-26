77a69478706d728533eee67df807708466f6889d
Kalyan Kanuri <kalyankanuri497@gmail.com>
2026-09-26 20:46:34 +0530

    fix log overwrite bug

--- projects/GoCart/internal/logger/logger.go
+++ projects/GoCart/internal/logger/logger.go
@@ -14,6 +14,8 @@ func New() zerolog.Logger {
 	zerolog.TimeFieldFormat = time.RFC3339
 	if os.Getenv("GIN_MODE") == "release" {
 		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339})
+		// log.Logger = log.Output(zerolog.ConsoleWriter{Out: , TimeFormat: time.RFC3339})
+		return log.Logger
 	}
 	log.Logger = log.Output(
 		zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339},
